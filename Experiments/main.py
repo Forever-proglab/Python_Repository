4c792b798b5ca09e47d5a5e6d9b48683430daf40
MarshalGW <136970742+Forever-proglab@users.noreply.github.com>
2026-06-29 17:19:27 +0500

    Update main.py

--- Experiments/main.py
+++ Experiments/main.py
@@ -1,9 +1,2 @@
-for p in range(100, 1000):
-        a = p // 100
-        b = (p % 100) // 10
-        c = p % 10
-        if (p - (a+b+c))/3==300:
-            print(p)
-        else:
-            continue
-print("No")
+import random
+print(round((random.uniform(1.5,-9.7)), 16))
