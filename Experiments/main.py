944f553bf5e196b496efcd7fc79fd3ce5aa5630b
MarshalGW <136970742+Forever-proglab@users.noreply.github.com>
2026-07-07 23:22:24 +0500

    Update main.py

---
+++ Experiments/main.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def main():
+    dict = []
+    m = int(input())
+    for i in range(1, m+1):
+        l = int(input())
+        for g in range(1, l+1):
+            k,u = int(input()), int(input())
+            
+
+
+
+if __name__ == '__main__':
+    main()
