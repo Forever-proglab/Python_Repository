e8562c1a2af029a6384c9b0af7991a14b70e2104
MarshalGW <136970742+Forever-proglab@users.noreply.github.com>
2026-08-25 21:44:53 +0500

    Update main.py

--- Experiments/main.py
+++ Experiments/main.py
@@ -1,18 +1,27 @@
-def main():
-    dict = {}
-    sum = 0
-    m = int(input())
-    for _ in range(1, m+1):
-        l = int(input())
-        for _ in range(1, l+1):
-            k,u = map(int, input().split())
-            sum += k
-            dict[k]=u
-    for e in range(0, sum+1):
-        if dict.get(e) != None:
-            print(e, dict[e])
-
-
-
-
-main()
+# def main():
+#     dict = {}
+#     sum = 0
+#     m = int(input())
+#     for _ in range(1, m+1):
+#         l = int(input())
+#         for _ in range(1, l+1):
+#             k,u = map(int, input().split())
+#             sum += k
+#             dict[k]=u
+#     for e in range(0, sum+1):
+#         if dict.get(e) != None:
+#             print(e, dict[e])
+# main()
+n = int(input())
+k=0
+for i in str(n):
+    k+=int(i)
+n=k
+print(k)
+while k>9:
+    k = 0
+    for i in str(n):
+        k+=int(i)
+    n=k
+    print(k)
+print(k)
