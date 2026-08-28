9b5a290b74241e5dfe7af5edbd26ac4c328d94b9
MarshalGW <136970742+Forever-proglab@users.noreply.github.com>
2026-08-28 17:44:02 +0500

    Update main.py

--- Experiments/main.py
+++ Experiments/main.py
@@ -26,11 +26,11 @@
 #     print(k)
 # print(k)
 t = False
-for e in range(0,151):
-    for a in range(0, 151):
-        for b in range(0, 151):
-            for c in range(0, 151):
-                for d in range(0, 151):
+for e in range(1,151):
+    for a in range(1, 151):
+        for b in range(1, 151):
+            for c in range(1, 151):
+                for d in range(1, 151):
                     if a**5 + b**5 + c**5 + d**5 == e**5:
                         print(a+b+c+d+e)
                         t = True
