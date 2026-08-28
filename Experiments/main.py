0fc73ce3d91a828749367bf4029c55210d559e40
MarshalGW <136970742+Forever-proglab@users.noreply.github.com>
2026-08-28 17:33:26 +0500

    Update main.py

--- Experiments/main.py
+++ Experiments/main.py
@@ -12,16 +12,24 @@
 #         if dict.get(e) != None:
 #             print(e, dict[e])
 # main()
-n = int(input())
-k=0
-for i in str(n):
-    k+=int(i)
-n=k
-print(k)
-while k>9:
-    k = 0
-    for i in str(n):
-        k+=int(i)
-    n=k
-    print(k)
-print(k)
+# n = int(input())
+# k=0
+# for i in str(n):
+#     k+=int(i)
+# n=k
+# print(k)
+# while k>9:
+#     k = 0
+#     for i in str(n):
+#         k+=int(i)
+#     n=k
+#     print(k)
+# print(k)
+for e in range(0,151):
+    for a in range(0, 151):
+        for b in range(0, 151):
+            for c in range(0, 151):
+                for d in range(0, 151):
+                    if a**1 + b**1 + c**1 + d**1 == e**1:
+                        print(a+b+c+d+e)
+
