328c7851c918343d9fb1f1a88e5f99123efc62f1
MarshalGW <136970742+Forever-proglab@users.noreply.github.com>
2026-09-24 20:26:15 +0500

    Update main.py

--- Experiments/main.py
+++ Experiments/main.py
@@ -46,85 +46,85 @@
 #             break
 #     if t:
 #         break
-con = 1
-s = 0
-while con != 0:
-    m, n, l, k = 0,0,0,0
-    m1, m2, m3=[],[],[]
-    print("This is matrix calculator! How many matrices you want to enter, one or two? Enter one or two")
-    u = input()
-    if u=='one':
-        m, n = int(input('Введите количество строк:')), int(input('Введите количество столбцов:'))
-        m1 = [[0 for _ in range(n)] for _ in range(m)]
-        for i in range(0, m):
-            for d in range(0, n):
-                y = input(f'Введите число {i+1} строки {d+1} столбца матрицы, если вы хотите ввести комплексное число, введите строку вида <вещ. часть>+<мнимая часть>j')
-                if 'j' in y:
-                    m1[i][d]=complex(y)
-                else:
-                    m1[i][d]=int(y)
-        for i in range(len(m1)):
-            print(m1[i])
-        print(f'{m}×{n}')
-        h = input('Введите число, на которое вы хотите умножить матрицу:')
-        if 'j' in h:
-            h1 = complex(h)
-        else:
-            h1 = int(h)
-        m3=[[elem * h1 for elem in row] for row in m1]
-        for i in range(len(m3)):
-            print(m3[i])
-        print(f'{m}×{n}')
-    elif u=='two':
-        m, n = int(input('Введите количество строк первой матрицы:')), int(input('Введите количество столбцов первой матрицы:'))
-        m1 = [[0 for _ in range(n)] for _ in range(m)]
-        for i in range(0, m):
-            for d in range(0, n):
-                y = input(f'Введите число {i+1} строки {d+1} столбца матрицы, если вы хотите ввести комплексное число, введите строку вида <вещ. часть>+<мнимая часть>j')
-                if 'j' in y:
-                    m1[i][d] = complex(y)
-                else:
-                    m1[i][d] = int(y)
-        l, k = int(input('Введите количество строк второй матрицы:')), int(input('Введите количество столбцов второй матрицы:'))
-        m2 = [[0 for _ in range(k)] for _ in range(l)]
-        for i in range(0, l):
-            for d in range(0, k):
-                y = input(f'Введите число {i+1} строки {d+1} столбца матрицы, если вы хотите ввести комплексное число, введите строку вида <вещ. часть>+<мнимая часть>j')
-                if 'j' in y:
-                    m2[i][d] = complex(y)
-                else:
-                    m2[i][d] = int(y)
-        for i in range(len(m1)):
-            print(m1[i])
-        print(f'{m}×{n}')
-        for i in range(len(m2)):
-            print(m2[i])
-        print(f'{l}×{k}')
-        c = input("What do you want to do with matrices? Enter + or - or *")
-        code1 = ([m1[i] for i in range(len(m1))])
-        code2 = ([m2[i] for i in range(len(m2))])
-        if c=='+':
-            if m==l and n==k:
-                for i in range(0, m):
-                    m3.append([(m1[i][d]+m2[i][d]) for d in range(0, n)])
-            else:
-                print(f'Матрицы {code1} и {code2} невозможно сложить')
-        elif c == '-':
-            if m == l and n == k:
-                for i in range(0, m):
-                    m3.append([(m1[i][d] - m2[i][d]) for d in range(0, n)])
-            print(f'Из матрицы {code1} невозможно вычесть матрицу {code2}')
-        elif c == '*':
-            if n==l:
-                m3 = [[0 for _ in range(k)] for _ in range(m)]
-                for i in range(m):
-                    for j in range(k):
-                        s = 0
-                        for h in range(n):
-                            s += m1[i][h] * m2[h][j]
-                        m3[i][j] = s
-            else:
-                print(f'Матрицы {code1} и {code2} невозможно умножить на друг друга')
-    for i in range(len(m3)):
-        print(m3[i])
-    con = int(input("Введите 0, если хотите выйти из программы, или любое другое целое число, если хотите продолжить пользоваться калькулятором"))
+# con = 1
+# s = 0
+# while con != 0:
+#     m, n, l, k = 0,0,0,0
+#     m1, m2, m3=[],[],[]
+#     print("This is matrix calculator! How many matrices you want to enter, one or two? Enter one or two")
+#     u = input()
+#     if u=='one':
+#         m, n = int(input('Введите количество строк:')), int(input('Введите количество столбцов:'))
+#         m1 = [[0 for _ in range(n)] for _ in range(m)]
+#         for i in range(0, m):
+#             for d in range(0, n):
+#                 y = input(f'Введите число {i+1} строки {d+1} столбца матрицы, если вы хотите ввести комплексное число, введите строку вида <вещ. часть>+<мнимая часть>j')
+#                 if 'j' in y:
+#                     m1[i][d]=complex(y)
+#                 else:
+#                     m1[i][d]=int(y)
+#         for i in range(len(m1)):
+#             print(m1[i])
+#         print(f'{m}×{n}')
+#         h = input('Введите число, на которое вы хотите умножить матрицу:')
+#         if 'j' in h:
+#             h1 = complex(h)
+#         else:
+#             h1 = int(h)
+#         m3=[[elem * h1 for elem in row] for row in m1]
+#         for i in range(len(m3)):
+#             print(m3[i])
+#         print(f'{m}×{n}')
+#     elif u=='two':
+#         m, n = int(input('Введите количество строк первой матрицы:')), int(input('Введите количество столбцов первой матрицы:'))
+#         m1 = [[0 for _ in range(n)] for _ in range(m)]
+#         for i in range(0, m):
+#             for d in range(0, n):
+#                 y = input(f'Введите число {i+1} строки {d+1} столбца матрицы, если вы хотите ввести комплексное число, введите строку вида <вещ. часть>+<мнимая часть>j')
+#                 if 'j' in y:
+#                     m1[i][d] = complex(y)
+#                 else:
+#                     m1[i][d] = int(y)
+#         l, k = int(input('Введите количество строк второй матрицы:')), int(input('Введите количество столбцов второй матрицы:'))
+#         m2 = [[0 for _ in range(k)] for _ in range(l)]
+#         for i in range(0, l):
+#             for d in range(0, k):
+#                 y = input(f'Введите число {i+1} строки {d+1} столбца матрицы, если вы хотите ввести комплексное число, введите строку вида <вещ. часть>+<мнимая часть>j')
+#                 if 'j' in y:
+#                     m2[i][d] = complex(y)
+#                 else:
+#                     m2[i][d] = int(y)
+#         for i in range(len(m1)):
+#             print(m1[i])
+#         print(f'{m}×{n}')
+#         for i in range(len(m2)):
+#             print(m2[i])
+#         print(f'{l}×{k}')
+#         c = input("What do you want to do with matrices? Enter + or - or *")
+#         code1 = ([m1[i] for i in range(len(m1))])
+#         code2 = ([m2[i] for i in range(len(m2))])
+#         if c=='+':
+#             if m==l and n==k:
+#                 for i in range(0, m):
+#                     m3.append([(m1[i][d]+m2[i][d]) for d in range(0, n)])
+#             else:
+#                 print(f'Матрицы {code1} и {code2} невозможно сложить')
+#         elif c == '-':
+#             if m == l and n == k:
+#                 for i in range(0, m):
+#                     m3.append([(m1[i][d] - m2[i][d]) for d in range(0, n)])
+#             print(f'Из матрицы {code1} невозможно вычесть матрицу {code2}')
+#         elif c == '*':
+#             if n==l:
+#                 m3 = [[0 for _ in range(k)] for _ in range(m)]
+#                 for i in range(m):
+#                     for j in range(k):
+#                         s = 0
+#                         for h in range(n):
+#                             s += m1[i][h] * m2[h][j]
+#                         m3[i][j] = s
+#             else:
+#                 print(f'Матрицы {code1} и {code2} невозможно умножить на друг друга')
+#     for i in range(len(m3)):
+#         print(m3[i])
+#     con = int(input("Введите 0, если хотите выйти из программы, или любое другое целое число, если хотите продолжить пользоваться калькулятором"))
